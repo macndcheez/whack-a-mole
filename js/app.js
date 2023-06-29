7f300cbe18fcfa42f59d7508e292020ecced2f1d
Nicole Kim <nicole@goat-735.lan>
2023-06-28 18:02:11 -0700

    set an interval for the squares and made the removing faster

--- js/app.js
+++ js/app.js
@@ -4,9 +4,13 @@ $(document).ready(function() {
     const $square = $('.square');
     // listen for clicks on start button
     $startBtn.on("click", function() {
-
+    // when clicked, call a function that  selects a random square and
+    // turns the backgroun green, and continues to call every 3 sec
+        setInterval(function () {
+            randomSquare();
+        }, 3000);
         randomSquare();
-    })
+    });
     // when clicked, call a function that selects a 
     //random square and turns the background green
 
@@ -16,9 +20,10 @@ $(document).ready(function() {
         const randomSquare = $square[Math.floor(Math.random() * $square.length)];
         $(randomSquare).addClass('green-bg');
 
+        // remove "green-bg" after 3 seconds (event loop)
         setTimeout(function() {
             $(randomSquare).removeClass('green-bg')
-        }, 3000)
+        }, 2000)
     }
 
     
