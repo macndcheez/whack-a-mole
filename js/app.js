7c3beb0caadab0d2af59b56490a9d3488a466923
Nicole Kim <nicole@goat-735.lan>
2023-06-28 18:23:15 -0700

    added point system to update and minor edits

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
     // select start button
     const $startBtn = $('#start');
+
+    // build a jquery array-like object of the squares
     const $square = $('.square');
+
+    // 
+    const $points = $('#points');
     // listen for clicks on start button
     $startBtn.on("click", function() {
     // when clicked, call a function that  selects a random square and
@@ -25,6 +30,28 @@ $(document).ready(function() {
             $(randomSquare).removeClass('green-bg')
         }, 2000)
     }
+    // add event listener to the squares
+    $square.on("click", function(event) {
+        // if a square is clicked and has green-bg
+      if ($(event.target).hasClass('green-bg')){
+          // immediately remove green-bg class
+          $(event.target).removeClass('green-bg')
+
+        
+        // add one point to scoreboard
+       // pulls current point value ($points.text())
+       // converts it to an integer (parseInt)
+        const newPointValue = parseInt($points.text()) + 1;
+        $points.text(newPointValue)
+
+      } else {
+        console.log("no points for you");
+      }
+    });
+    // add one point to the leaderboard
+
+
+
 
     
 })
