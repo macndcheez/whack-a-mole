d8c8544e2670bf9c6b06f2ae757cdb2ec03186a4
Nicole Kim <nicole@goat-735.lan>
2023-06-28 17:55:28 -0700

    working random green square then disappear

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-    console.log("i'm ready!")
+    // select start button
+    const $startBtn = $('#start');
+    const $square = $('.square');
+    // listen for clicks on start button
+    $startBtn.on("click", function() {
+
+        randomSquare();
+    })
+    // when clicked, call a function that selects a 
+    //random square and turns the background green
+
+    // write a function that randomly selects a square and 
+    // adds a class to turn the background green
+    function randomSquare() {
+        const randomSquare = $square[Math.floor(Math.random() * $square.length)];
+        $(randomSquare).addClass('green-bg');
+
+        setTimeout(function() {
+            $(randomSquare).removeClass('green-bg')
+        }, 3000)
+    }
+
+    
 })
